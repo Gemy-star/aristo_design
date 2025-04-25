3e9204af5dda0508958a37a8587fe828e15516a9
Mohamed Ismail <mohamed.g.ismail@orange.com>
2025-04-26 01:23:42 +0300

    finish most of website

--- aristo/urls.py
+++ aristo/urls.py
@@ -1,17 +1,21 @@
-from django.contrib import admin
-from django.urls import path, include
 from django.conf import settings
-from django.conf.urls.static import static
+from django.conf.urls import include
+from django.contrib import admin
+from django.urls import path
 from django.conf.urls.i18n import i18n_patterns
+from django.conf.urls.static import static
 
 urlpatterns = [
+    # Internationalization URLs for language switching
     path('i18n/', include('django.conf.urls.i18n')),
 ]
 
+# Adding internationalized URLs to the urlpatterns
 urlpatterns += i18n_patterns(
-    path('admin/', admin.site.urls),
-    path('', include('main.urls')),  # <-- لازم يحتوي على path('')
+    path('admin/', admin.site.urls),  # Admin URL
+    path('', include('main.urls')),  # Your main application URLs
 )
 
+# For serving static and media files during development (when DEBUG is True)
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- main/translation.py
+++ main/translation.py
@@ -1,7 +1,11 @@
 from modeltranslation.translator import translator, TranslationOptions
-from .models import HomeSlider
+from .models import HomeSlider , Announcement
 
 class HomeSliderTranslationOptions(TranslationOptions):
     fields = ('alt_text', 'heading', 'subheading', 'button_text')
 
+class AnnouncementTranslationOptions(TranslationOptions):
+    fields = ('title', 'content')
+
 translator.register(HomeSlider, HomeSliderTranslationOptions)
+translator.register(Announcement, AnnouncementTranslationOptions)
